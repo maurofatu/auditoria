61021ab52fea3b0bb386ffa3a939d79dd74efaec
David Diaz <hddiazv@gmail.com>
2022-03-09 18:01:15 -0500

    repare bug in FactVotesController

--- app/Http/Controllers/FactVoteController.php
+++ app/Http/Controllers/FactVoteController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\FactVoteRequest;
-use Illuminate\Support\Facades\Auth;
 
 class FactVoteController extends Controller
 {
