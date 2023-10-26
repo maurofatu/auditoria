a259c5982860f30bcddb383da20bac6820b34bad
David Diaz <hddiazv@gmail.com>
2023-10-26 02:07:30 -0500

    novedades

---
+++ resources/views/coordinators/find.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        <div class="row">
+            <div class="col-xl-12 col-sm-12 mb-xl-0 mb-2">
+                <div class="card" style="background-color: #0CA789;color:white;">
+                    <div class="card-body p-2">
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <b>Elecciones Regionales 2023</b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 text-center mt-2">
+                <h4>Monitoreo Novedades - Coordinador de Puesto</h4>
+            </div>
+            <div class="col-12 mt-2">
+                <h5>Municipio: {{ $data['fact_permits']->factPollingStation->dimCity->description }}</h5>
+                <h5>Puesto: {{ $data['fact_permits']->factPollingStation->dimLocation->description }}</h5>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Hora</th>
+                        <th scope="col">Tipo Novedad</th>
+                        <th scope="col">Estado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($data['fact_news'] as $new)
+                        <tr>
+                            <th scope="row">{{ $new->id }}</th>
+                            <td>{{ $new->hora }}</td>
+                            <td>{{ $new->type }}</td>
+                            <td>{{ $new->status }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <th scope="row" colspan="4">Sin Datos</th>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+            <div class="text-center">
+                <ul class="pagination">
+                    @if ($data['fact_news']->onFirstPage())
+                        <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
+                    @else
+                        <li class="page-item"><a class="page-link" href="{{ $data['fact_news']->previousPageUrl() }}"
+                                rel="prev">&laquo;</a></li>
+                    @endif
+
+                    @foreach ($data['fact_news']->getUrlRange(1, $data['fact_news']->lastPage()) as $page => $url)
+                        @if ($page == $data['fact_news']->currentPage())
+                            <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
+                        @else
+                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                            </li>
+                        @endif
+                    @endforeach
+
+                    @if ($data['fact_news']->hasMorePages())
+                        <li class="page-item"><a class="page-link" href="{{ $data['fact_news']->nextPageUrl() }}"
+                                rel="next">&raquo;</a></li>
+                    @else
+                        <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
+                    @endif
+                </ul>
+            </div>
+
+        </div>
+        <div class="row">
+            <p class="m-0"><strong>Total novedades:</strong> {{$data['conteo']->S + $data['conteo']->G + $data['conteo']->D }}</p>
+            <p class="m-0 text-danger"><strong>Sin gestionar:</strong> {{ $data['conteo']->S }}</p>
+            <p class="m-0 text-primary"><strong>Gestionados:</strong> {{ $data['conteo']->G }}</p>
+            <p class="m-0 text-success"><strong>Direccionados:</strong> {{ $data['conteo']->D }}</p>
+        </div>
+
+    </div> <!-- CONTAINER -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="{{ asset('js/function.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.js-example-basic-single').select2();
+            $('.js-example-basic-single2').select2();
+            // document.getElementById("ndivPotentialVotes").hide();
+            $("#ndivPotentialVotes").hide();
+
+        });
+    </script>
+@endsection
--- resources/views/coordinators/index.blade.php
+++ resources/views/coordinators/index.blade.php
@@ -1,60 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <div class="container">
 
-    <div class="row">
-        <div class="col-xl-12 col-sm-12 mb-xl-0 mb-2">
-            <div class="card" style="background-color: #0CA789;color:white;">
-                <div class="card-body p-2">
-                    <div class="row">
-                        <div class="col-12 text-center">
-                            <b>Elecciones Regionales 2023</b>
+        <div class="row">
+            <div class="col-xl-12 col-sm-12 mb-xl-0 mb-2">
+                <div class="card" style="background-color: #0CA789;color:white;">
+                    <div class="card-body p-2">
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <b>Elecciones Regionales 2023</b>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-12 text-center mt-2">
-            <h4>Monitoreo Novedades - Coordinador de Puesto</h4>
+        <div class="row">
+            <div class="col-12 text-center mt-2">
+                <h4>Monitoreo Novedades - Coordinador de Puesto</h4>
+            </div>
+            <div class="col-12 mt-2">
+                <h5>Municipio: {{ $data['fact_permits']->factPollingStation->dimCity->description }}</h5>
+                <h5>Puesto: {{ $data['fact_permits']->factPollingStation->dimLocation->description }}</h5>
+            </div>
         </div>
-        <div class="col-12 mt-2">
-            <h5>Municipio: {{$data['fact_permits']->factPollingStation->dimCity->description}}</h5>
-            <h5>Puesto: {{$data['fact_permits']->factPollingStation->dimLocation->description}}</h5>
+
+        <div class="row justify-content-center">
+            <div class="col-8">
+                @forelse ($data['fact_polling_station'] as $polling_station)
+                    <a href="{{ route('coordinators.find', ['city' => $polling_station->fk_dim_cities, 'location' => $polling_station->fk_dim_locations, 'table' => $polling_station->fk_dim_tables]) }}"
+                        type="button"
+                        class="btn {{ $polling_station->cantidad_s > 0 ? 'btn-danger' : ($polling_station->cantidad_g > 0 ? 'btn-primary' : ($polling_station->cantidad_d > 0 ? 'btn-success' : 'btn-outline-dark')) }} m-1 px-3 rounded-pill">{{ ($polling_station->fk_dim_tables <= 9 ? '0' : '') . $polling_station->fk_dim_tables }}</a>
+                @empty
+                    No hay datos
+                @endforelse
+            </div>
         </div>
-    </div>
-
-    <div class="row justify-content-center">
-        <div class="col-8">
-            @forelse ($data['fact_polling_station'] as $polling_station)
-            <a href="#" type="button" class="btn {{ (($polling_station->cantidad_s) > 0 ? 'btn-danger' : ( (($polling_station->cantidad_g) > 0) ? 'btn-primary' : ( ($polling_station->cantidad_d) > 0 ? 'btn-success' : 'btn-outline-dark' ) ) )}} m-1 px-3 rounded-pill">{{ (($polling_station->fk_dim_tables) <= 9 ? '0' : '' ) . $polling_station->fk_dim_tables}}</a>
-            @empty
-            No hay datos
-            @endforelse
+        <div class="row">
+            <p class="m-0">Color:</p>
+            <p class="m-0">Blanco: Mesas sin novedades.</p>
+            <p class="m-0">Rojo = mesa con novedades sin gestionar.</p>
+            <p class="m-0">Azul: Novedades gestionadas</p>
+            <p class="m-0">Verde: Novedades gestionadas y direccionadas</p>
         </div>
-    </div>
-    <div class="row">
-        <p class="m-0">Color:</p>
-        <p class="m-0">Blanco: Mesas sin novedades.</p>
-        <p class="m-0">Rojo = mesa con novedades sin gestionar.</p>
-        <p class="m-0">Azul: Novedades gestionadas</p>
-        <p class="m-0">Verde: Novedades gestionadas y direccionadas</p>
-    </div>
 
-</div> <!-- CONTAINER -->
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script src="{{ asset('js/function.js') }}"></script>
+    </div> <!-- CONTAINER -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="{{ asset('js/function.js') }}"></script>
 
-<script>
-    $(document).ready(function() {
-        $('.js-example-basic-single').select2();
-        $('.js-example-basic-single2').select2();
-        // document.getElementById("ndivPotentialVotes").hide();
-        $("#ndivPotentialVotes").hide();
+    <script>
+        $(document).ready(function() {
+            $('.js-example-basic-single').select2();
+            $('.js-example-basic-single2').select2();
+            // document.getElementById("ndivPotentialVotes").hide();
+            $("#ndivPotentialVotes").hide();
 
-    });
-</script>
+        });
+    </script>
 @endsection
